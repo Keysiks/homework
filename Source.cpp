2a091a187af02a39f48ca787de4de9cec6c6aec6
Keysiks <112818351+Keysiks@users.noreply.github.com>
2022-09-23 12:00:45 +0300

    сделанные пятнашки

--- Source.cpp
+++ Source.cpp
@@ -1,78 +1,84 @@
-#include<iostream>
-#include<string>
-#include<conio.h>
-#include<random>
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <random>
+#include <Windows.h>
 using namespace std;
 
+
 void main() {
-	string name1;
-	cout << "Enter name: "; cin >> name1; 
-	int ideal[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	int start[16] = {0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	int random; bool f = true;
-	for (int i = 0; i < 16; i++) 
-		start[i] = 17;
-	for (int i = 0; i < 15; i++)
-		ideal[i] = i + 1;
-	for (int i = 0; i < 16; i++) {
-		random = rand() % 16;
-		for (int j = 0; j < 16; j++) {
-			if (start[j] == random) {
-				f = false;
-				break;
+	int field[4][4] = { {9, 5, 7, 8}, {0, 2, 6, 1}, {14, 12, 3, 11}, {4, 10, 15, 13} };
+	int ideal[4][4] = { {1, 2, 3, 4 }, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0} };
+
+	int counter = 0;
+	int move, position_x = 0, position_y = 1;
+	int i0, j0;
+	while (ideal != field) {
+		setlocale(LC_ALL, "");
+		system("cls");
+		cout << "Игра в пятнашки." << endl;
+		cout << "Правила: " << endl;
+		cout << "Вы можете двигать пустую клетку комбинацией клавиш WASD, за перделы поля выходить нельзя." << endl;
+		cout << "Игра заканчивается, когда все числа упорядочены по возврастанию." << endl;
+		cout << "\t" << "\t";
+		cout << endl;
+		cout << "\t\t";
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				cout << field[i][j] << "  ";
+				if (field[i][j] < 10) {
+					cout << " ";
+				}
 			}
+			cout << endl << endl << "\t\t";
 		}
-		while (not f) {
-			f = true;
-			random = rand() % 16;
-			for (int j = 0; j < 16; j++) {
-				if (start[j] == random) {
-					f = false;
+		move = _getch();
+		switch (move) {
+		case -32: break;
+		case 'w':
+			if (position_y == 0)
+				cout << "Enter other key" << endl;
+			else
+				position_y -= 1;
+			break;
+		case 's':
+			if (position_y == 3)
+				cout << "Enter other key" << endl;
+			else
+				position_y += 1;
+			break;
+		case 'd':
+			if (position_x == 3)
+				cout << "Enter other key" << endl;
+			else
+				position_x += 1;
+			break;
+		case 'a':
+			if (position_x == 0)
+				cout << "Enter other key" << endl;
+			else
+				position_x -= 1;
+			break;
+		default:
+			break;
+		}
+		Sleep(100);
+		int x = field[position_y][position_x];
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (field[i][j] == 0) {
+					j0 = j;
+					i0 = i;
 					break;
 				}
 			}
 		}
-		start[i] = random;
-		f = true;
+		field[position_y][position_x] = 0;
+		field[i0][j0] = x;
+		counter += 1;
 	}
-	for (int i = 0; i < 16; i++)
-		cout << start[i] << " ";
+	setlocale(LC_ALL, "");
 	cout << endl;
-	int move;
-	int x = 1, i0;
-	while (start != ideal) {
-		move = _getch();
-		switch (move) {
-			case 77:
-				x += 1;
-				if (x > 16) {
-					x = 16;
-					cout << "You exited from field";
-				}
-				cout << "Position = " << x << "\t" << start[x - 1] << endl;
-				break;
-			case 75:
-				x -= 1;
-				if (x < 1) {
-					x = 1;
-					cout << "You exited from field";
-				}
-				cout << "Position = " << x << "\t" << start[x - 1] << endl;
-				break;
-			case 13:
-				for (int i = 0; i < 16; i++) {
-					if (start[i] == 0)
-						i0 = i;
-				}
-				start[i0] = start[x - 1];
-				start[x - 1] = 0;
-				for (int i = 0; i < 16; i++) {
-					cout << start[i] << " ";
-				}
-				cout << endl;
-				break;
-		}
-	}
-	cout << name1 << " ,Поздравляем!";
-
+	cout << "Количество ходов: " << counter << endl;
+	cout << "Поздравляем! Вы победили!";
 }
